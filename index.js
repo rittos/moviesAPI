4791a740aabbaf314055d38cae695c947a86c876
Ritto Poovathingal Thimothy <20096059@mail.wit.ie>
2022-04-25 18:47:20 +0100

    moviesAPI: Updated Movies Endpoints

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import moviesRouter from './src/movies';
+import createAccountsRouter from './src/accounts/routes';
+import dependencies from "./src/config/dependencies";
+import createMoviesRouter from './src/movies/routes';
 
 dotenv.config();
 
 const app = express();
 
 const port = process.env.PORT;
+// const dependencies = buildDependencies();
 
 app.use('/api/movies', moviesRouter);
+app.use('/api/accounts', createAccountsRouter(dependencies));
+app.use('/api/movies', createMoviesRouter(dependencies));
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
